6b729903ce43e7354aeb8f7a03b34367c6a17642
ruoxi512 <suruoxi@gmail.com>
2021-10-30 10:44:21 +0800

    second commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-//#include "mythread.h"
 #include <QDebug>
 #include <QProcess>
 #include <QMessageBox>
 #include <QFile>
 #include <QDir>
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,8 +18,11 @@ MainWindow::MainWindow(QWidget *parent)
     connect(timer,SIGNAL(timeout()),this,SLOT(readFrame()));
     connect(ui->Open,SIGNAL(clicked()),this,SLOT(on_Open_triggered()));
     connect(ui->Stop,SIGNAL(clicked()),this,SLOT(on_Stop_triggered()));
+    connect(ui->Exit,SIGNAL(clicked()),this,SLOT(on_Exit_triggered()));
     connect(ui->modeswitch,SIGNAL(clicked()),this,SLOT(on_modeswitch_clicked()));
-    connect(ui->lineEdit,SIGNAL(textChanged(QString)),this,SLOT(readport()));
+    connect(ui->Photograph,SIGNAL(clicked()),this,SLOT(on_Photograph_clicked()));
+    connect(ui->lineEdit1,SIGNAL(textChanged(QString)),this,SLOT(readport()));
+    connect(ui->lineEdit2,SIGNAL(textChanged(QString)),this,SLOT(readmode()));
     connect(process , &QProcess::readyReadStandardOutput, this , &MainWindow::readoutput);
     connect(process , &QProcess::readyReadStandardError, this , &MainWindow::readerror);
 }
@@ -44,27 +47,132 @@ void MainWindow::on_Stop_triggered()
     ui->cameraView->clear();
 }
 
+void MainWindow::on_About_triggered()
+{
+    QMessageBox::about(this, tr("About Application"),
+             tr("The <b>Application</b> example demonstrates how to "
+                "write modern GUI applications using Qt, with a menu bar, "
+                "toolbars, and a status bar."));
+}
+
+void MainWindow::on_Exit_triggered()
+{
+    exit(0);
+}
+
+void MainWindow::on_Photograph_clicked()
+{
+    QString timeStr = QDateTime::currentDateTime().toString("yyyy-MM-hh hh_mm_ss");
+    QString dirName = "Pictures";
+    QString homepath = QDir::homePath();
+    QImage image = MatImageToQt(src_image);
+    if(!QFile::exists(dirName))
+    {
+       QDir dir;
+       if(!dir.mkdir(dirName))
+       {
+           ui->label_state->setText("创建文件夹 " + dirName + "失败!!!");
+//           qDebug() << __FILE__ << __LINE__ << "创建文件夹 " << dirName << "失败!!!";
+       }
+    }
+    QString filePath = QString("%1/%2/%3.png").arg(homepath).arg(dirName).arg(timeStr);
+    if(image.save(filePath))
+    {
+//        qDebug() << __FILE__ << __LINE__ << "保存照片至: " << filePath;
+        ui->label_state->setText("保存照片至: " + filePath);
+    }else{
+        ui->label_state->setText("保存照片失败!!!");
+//        qDebug() << __FILE__ << __LINE__ << "保存照片失败!!!";
+    }
+}
+
 void MainWindow::on_modeswitch_clicked()
 {
     process->start("bash");                      //启动终端(Windows下改为cmd)
     process->waitForStarted();                   //等待启动完成
-//    QString homePath = QDir::currentPath();;
-//    QFile file(homePath + "/camera_switch.sh");
-//    QFile file1(":/new/prefix1/camera_switch.sh\n");
-//    qDebug()<<file1 << endl;
-//    process->write(file.readAll());    //读文件,默认只识别UTF-8,向终端写入命令，注意尾部的“\n”不可省略
-    process->write("/home/sujie/Workspace/QT/webcam_Stereo/camera_switch.sh 2 4\n");    //向终端写入命令，注意尾部的“\n”不可省略
+    //    process->write("/home/sujie/Workspace/QT/webcam_Stereo/camera_switch.sh 2 4\n");    //向终端写入命令，注意尾部的“\n”不可省略
+    // 切换相机输出模式
+    //QString的arg()方法用于填充字符串中的%1,%2...为给定的参数
+    QString str1=tr("uvcdynctrl -d /dev/video%1 -S 6:8  '(LE)0x50ff' ").arg(device);
+    QString str2=tr("uvcdynctrl -d /dev/video%1 -S 6:15 '(LE)0x00f6' ").arg(device);
+    QString str3=tr("uvcdynctrl -d /dev/video%1 -S 6:8  '(LE)0x2500' ").arg(device);
+    QString str4=tr("uvcdynctrl -d /dev/video%1 -S 6:8  '(LE)0x5ffe' ").arg(device);
+    QString str5=tr("uvcdynctrl -d /dev/video%1 -S 6:15 '(LE)0x0003' ").arg(device);
+    QString str6=tr("uvcdynctrl -d /dev/video%1 -S 6:15 '(LE)0x0002' ").arg(device);
+    QString str7=tr("uvcdynctrl -d /dev/video%1 -S 6:15 '(LE)0x0012' ").arg(device);
+    QString str8=tr("uvcdynctrl -d /dev/video%1 -S 6:15 '(LE)0x0004' ").arg(device);
+    QString str9=tr("uvcdynctrl -d /dev/video%1 -S 6:8  '(LE)0x76c3' ").arg(device);
+    QString str10=tr("uvcdynctrl -d /dev/video%1 -S 6:10 '(LE)0x0%200'").arg(device).arg(mode);
+    QString array[10]={str1,str2,str3,str4,str5,str6,str7,str8,str9,str10};
+    char *n;
+    for(int i=0;i<10;i++){
+        QByteArray m=array[i].toLatin1();
+        n=m.data();
+        process->write(n);
+        process->write("\n");
+    }
+}
+
+void MainWindow::on_record_clicked()
+{
+    QString timeStr = QDateTime::currentDateTime().toString("yyyy-MM-hh hh_mm_ss");
+    QString dirName = "Videos";
+    QString homepath = QDir::homePath();
+    double fps = 25.0;  // framerate of the created video stream
+    if(!QFile::exists(dirName))
+    {
+       QDir dir;
+       if(!dir.mkdir(dirName))
+       {
+           ui->label_state->setText("创建文件夹 " + dirName + "失败!!!");
+       }
+    }
+    QString filePath = QString("%1/%2/%3.avi").arg(homepath).arg(dirName).arg(timeStr);
+    cap.open(device.toInt());
+    writer.open(filePath.toStdString(), VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, Size(640, 480), true);
+    // check if we succeeded
+    if(!writer.isOpened()) {
+            cerr << "Could not open the output video file for write\n";
+        }
+    while(!src_image.empty())
+       {
+           // check if we succeeded
+           if (!cap.read(src_image)) {
+               cerr << "ERROR! blank frame grabbed\n";
+               break;
+           }
+           cap >> src_image;
+           //设置保存视频的格式为AVI，编码为MJPG
+           writer.write(src_image);
+//           namedWindow("VideoPlay", WINDOW_NORMAL);
+           imshow("VideoPlay", src_image);
+           if (waitKey(5) >= 0)
+               break;
+           if(!writer.isOpened()) {
+               ui->label_state->setText("保存视频至: " + filePath);
+               break;
+             }
+       }
+}
+
+void MainWindow::on_pause_clicked()
+{
+    // 释放资源，清空缓存
+//    destroyWindow("VideoPlay");
+    destroyAllWindows();
+    writer.release();
 }
 
 void MainWindow::readport()
 {
-    this->device=ui->lineEdit->text();
-    QString str=QString("/home/sujie/Workspace/QT/webcam_Stereo/camera_switch.sh %1\n").arg(device);
-    char *n;
-    QByteArray m=str.toLatin1();
-    n=m.data();
-    process->write(n);
+    this->device=ui->lineEdit1->text();
 }
+
+void MainWindow::readmode()
+{
+    this->mode=ui->lineEdit2->text();
+}
+
 void MainWindow::readoutput()
 {
     ui->textEdit->append(process->readAllStandardOutput().data());   //将输出信息读取到编辑框
--- mainwindow.h
+++ mainwindow.h
@@ -5,9 +5,9 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/videoio.hpp"
 #include <QTimer>
 #include <QProcess>
-//#include "mythread.h"
 
 QT_BEGIN_NAMESPACE
 
@@ -31,10 +31,16 @@ private slots:
     void readFrame();
     void on_Open_triggered();
     void on_Stop_triggered();
+    void on_About_triggered();
+    void on_Exit_triggered();
     void on_modeswitch_clicked();
     void readoutput();
     void readerror();
     void readport();
+    void readmode();
+    void on_Photograph_clicked();
+    void on_record_clicked();
+    void on_pause_clicked();
 
 private:
     Ui::MainWindow *ui;
@@ -44,6 +50,8 @@ private:
     QImage *image;
     QProcess *process;
     QString device; //相机索引
+    QString mode; //模式代号
+    VideoWriter writer;
 };
 
 #endif // MAINWINDOW_H
